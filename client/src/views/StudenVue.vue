a4067c4a34afcaefa186b81f11841a85f573874b
Abhinand <abhinandkofficial01@gmail.com>
2023-04-03 13:56:38 +0530

    sass variable file created

--- client/src/views/StudenVue.vue
+++ client/src/views/StudenVue.vue
@@ -1,32 +1,49 @@
 <template>
-    <h1>Hello</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form>
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required>
+
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="phone">Phone number:</label>
+                    <input type="tel" id="phone" name="phone" required>
+
+                    <label for="age">Age:</label>
+                    <input type="number" id="age" name="age" required>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
 import '@/assets/styles/index.scss'
+import '@/assets/styles/_variables.scss'
 import Student from '@/types/student'
 
 export default defineComponent({
-  name: 'StudentVue',
+    name: 'StudentVue',
 
-  setup() {
-    const studentDetails = reactive<Student>({
-      name: '',
-      email: '',
-      age: 0,
-      phNumber: 0
-    })
+    setup() {
+        const studentDetails = reactive<Student>({
+            name: '',
+            email: '',
+            age: 0,
+            phNumber: 0
+        })
 
-    return {
-      studentDetails
+        return {
+            studentDetails
+        }
     }
-  }
 })
 </script>
 
 <style lang="scss">
-    h1 {
-        color: red;
-    }
+
 </style>
