11ba25d9808fa790b64a5ff146198254669573be
Abhinand <abhinandkofficial01@gmail.com>
2023-04-03 12:44:52 +0530

    style file created and imported to StudentVue

--- client/src/views/StudenVue.vue
+++ client/src/views/StudenVue.vue
@@ -1,12 +1,21 @@
 <template>
-    <h1>Hi Abhi!!!</h1>
+    <h1>Hello</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent} from 'vue'
+import '@/assets/styles/index.scss'
 
 export default defineComponent({
-  name: 'StudentVue'
+  name: 'StudentVue',
+
+
 
 })
 </script>
+
+<style lang="scss">
+    h1 {
+        color: red;
+    }
+</style>
