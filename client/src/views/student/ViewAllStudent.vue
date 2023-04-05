a6513729813d5ee8a47fcaf9ee3adaaedfa0e0ae
Abhinand <abhinandkofficial01@gmail.com>
2023-04-05 12:20:19 +0530

    table created

--- client/src/views/student/ViewAllStudent.vue
+++ client/src/views/student/ViewAllStudent.vue
@@ -1,5 +1,28 @@
 <template>
-  <p>Hi</p>
+  <div class="container">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>@mdo</td>
+      <td>
+        <button>Remove</button>
+      </td>
+      <td>
+        <button>Edit</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+  </div>
 </template>
 
 <script lang="ts">
