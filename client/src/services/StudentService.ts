1512f52c4c25c6a4ad14d2786472de3d7d670570
Abhinand <abhinandkofficial01@gmail.com>
2023-04-07 11:49:42 +0530

    set return type Promise<any>

--- client/src/services/StudentService.ts
+++ client/src/services/StudentService.ts
@@ -1,30 +1,31 @@
 import axios from "axios";
-import Student from "@/types/student";
+import { Student } from "@/types/student";
+
 
 export default class StudentService {
     static URL = "http://localhost:3000/api/post"
 
-    static fetchAllStudent() {
+    static fetchAllStudent(): Promise<any> {
         const dataURL = `${this.URL}`
-        return axios.get<Student>(dataURL)
+        return axios.get(dataURL)
     }
 
-    static fetchStudent(studentId: number) {
+    static fetchStudent(studentId: number): Promise<any> {
         const dataURL = `${this.URL}/${studentId}`
         return axios.get(dataURL)
     }
 
-    static createStudent(student: Student) {
+    static createStudent(student: Student): Promise<any> {
         const dataURL = `${this.URL}`
         return axios.post(dataURL, student)
     }
 
-    static deleteStudent(studentId: number) {
+    static deleteStudent(studentId: any) : Promise<any> {
         const dataURL = `${this.URL}/${studentId}`
         return axios.delete(dataURL);
     }
 
-    static updateStudent(studentId: number) {
+    static updateStudent(studentId: number): Promise<any> {
         const dataURL = `${this.URL}/${studentId}`
         return axios.put(dataURL);
     }
