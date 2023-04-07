52e7ac0344019b36e66c0045e4423a9c26942764
Abhinand <abhinandkofficial01@gmail.com>
2023-04-07 11:51:49 +0530

    removed default

--- client/src/types/student.ts
+++ client/src/types/student.ts
@@ -1,8 +1,8 @@
-interface Student {
-    name: string,
-    email: string,
-    age: number,
-    place: string
-}
-
-export default Student
+export interface Student {
+    id: any;
+    name: string;
+    email: string;
+    age: number;
+    place: string;
+  }
+  
