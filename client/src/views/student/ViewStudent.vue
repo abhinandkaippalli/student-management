4704d2f6a6cb3d792321a3f91719ca16caf60451
Abhinand <abhinandkofficial01@gmail.com>
2023-04-11 13:00:23 +0530

    student details

--- client/src/views/student/ViewStudent.vue
+++ client/src/views/student/ViewStudent.vue
@@ -1,5 +1,17 @@
 <template>
-  <p>asdfghjkl</p>
+  <div class="container mt-5">
+    <div class="row justify-center">
+      <div class="col">
+        <ul class="list-group-secondary">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
